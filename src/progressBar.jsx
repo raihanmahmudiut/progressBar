434f020c2490ad877dc0e1d389c5c914647cf7b6
Raihan Mahmud <raihanmahmudiut@gmail.com>
2024-02-24 19:42:10 +0600

    addded validation check for milestone value button

--- src/progressBar.jsx
+++ src/progressBar.jsx
@@ -3,56 +3,64 @@ import { Box, Progress, Text, Icon, Divider } from "@chakra-ui/react";
 import { CheckIcon } from "@chakra-ui/icons";
 
 function ProgressBar({
-  milestones,
-  currentProgress,
-  calculateProgress,
-  progress,
+	milestones,
+	currentProgress,
+	calculateProgress,
+	progress,
 }) {
-  useEffect(() => {
-    calculateProgress();
-  }, [milestones, currentProgress]);
+	useEffect(() => {
+		calculateProgress();
+	}, [milestones, currentProgress]);
 
-  return (
-    <Box textAlign="center" position="relative">
-      <Text>
-        Milestones:{" "}
-        {milestones.map((milestone) => milestone.value).join(", ")}
-      </Text>
-      <Divider my={10} />
-      <Progress value={progress} colorScheme="green" />
+	const milestonesAvailable = milestones.length !== 0;
+	return (
+		<Box textAlign="center" position="relative">
+			<Text>
+				Milestones:{" "}
+				{milestonesAvailable &&
+					milestones.map((milestone) => milestone.value).join(", ")}
+			</Text>
 
-      {milestones.map((milestone, index) => {
-        const milestoneCompleted = currentProgress >= milestone.value;
+			<Divider my={10} />
+			<Progress value={progress} colorScheme="green" />
 
-        return (
-          <Box
-            key={index}
-            position="absolute"
-            left={`calc(${(index + 1) / (milestones.length + 1) * 100}% - 12px)`}
-            top="calc(95%)"
-            transform="translateY(-50%)"
-            zIndex="1"
-            textAlign="center"
-          >
-            <Box
-              borderRadius="full"
-              borderWidth="1px"
-              borderColor= "green.500"
-              bg={milestoneCompleted ? "green.500" : "white"}
-              color={milestoneCompleted ? "white" : "green.500"}
-              h="2rem"
-              w="2rem"
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-            >
-              <Icon as={CheckIcon} color={milestoneCompleted ? "white" : "green.500"} />
-            </Box>
-          </Box>
-        );
-      })}
-    </Box>
-  );
+			{milestones.map((milestone, index) => {
+				const milestoneCompleted = currentProgress >= milestone.value;
+
+				return (
+					<Box
+						key={index}
+						position="absolute"
+						left={`calc(${
+							((index + 1) / (milestones.length + 1)) * 100
+						}% - 12px)`}
+						top="calc(95%)"
+						transform="translateY(-50%)"
+						zIndex="1"
+						textAlign="center"
+					>
+						<Box
+							borderRadius="full"
+							borderWidth="1px"
+							borderColor="green.500"
+							bg={milestoneCompleted ? "green.500" : "white"}
+							color={milestoneCompleted ? "white" : "green.500"}
+							h="2rem"
+							w="2rem"
+							display="flex"
+							alignItems="center"
+							justifyContent="center"
+						>
+							<Icon
+								as={CheckIcon}
+								color={milestoneCompleted ? "white" : "green.500"}
+							/>
+						</Box>
+					</Box>
+				);
+			})}
+		</Box>
+	);
 }
 
 export default ProgressBar;
